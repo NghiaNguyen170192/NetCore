01b963067ecb16a3fe915aac4552af624e3c4794
Nguyen Quoc Trong nghia <nghia.nguyen170192@gmail.com>
2022-05-18 11:39:54 +0700

    update nuget

--- src/client/NetCore.Api/Controllers/AuthorizedBaseController.cs
+++ src/client/NetCore.Api/Controllers/AuthorizedBaseController.cs
@@ -1,12 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace NetCore.Api.Controllers
 {
 
-#if RELEASE
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Authorize(Roles = "user")]
-#endif
     [ApiController]
     [Route("api/[controller]")]
     public class AuthorizedBaseController : ControllerBase
