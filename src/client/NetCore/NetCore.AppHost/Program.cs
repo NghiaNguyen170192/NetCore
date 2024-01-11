157258d6e6b09b23c370031c88f2edea89598f6e
Nguyen Quoc Trong Nghia <nghia.nguyen170192@gmail.com>
2024-01-11 10:29:22 +0700

    introduce aspire to solution and projects

--- src/client/NetCore/NetCore.AppHost/Program.cs
+++ src/client/NetCore/NetCore.AppHost/Program.cs
@@ -1,3 +1,10 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.Build().Run();
+var redis = builder.AddRedisContainer("redis");
+var mssql = builder.AddSqlServerContainer("mssql");
+
+var netcoreApi = builder
+    .AddProject<Projects.NetCore_Api>("netcore-api")
+    .WithReference(mssql);
+
+await builder.Build().RunAsync();
