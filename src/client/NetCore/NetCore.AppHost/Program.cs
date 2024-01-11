9b76958149136fd577abe4a38da733c04919264c
Nguyen Quoc Trong Nghia <nghia.nguyen170192@gmail.com>
2024-01-11 13:11:54 +0700

    update

--- src/client/NetCore/NetCore.AppHost/Program.cs
+++ src/client/NetCore/NetCore.AppHost/Program.cs
@@ -1,10 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var redis = builder.AddRedisContainer("redis");
-var mssql = builder.AddSqlServerContainer("mssql");
-
-var netcoreApi = builder
-    .AddProject<Projects.NetCore_Api>("netcore-api")
-    .WithReference(mssql);
+var netcoreApi = builder.AddProject<Projects.NetCore_Api>("netcore-api");
 
 await builder.Build().RunAsync();
